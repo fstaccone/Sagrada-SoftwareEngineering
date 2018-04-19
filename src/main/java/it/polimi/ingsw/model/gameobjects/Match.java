3d59c77f56abc69fb441283ffb5b00baf3c9df00
francescostaccone <francesco0staccone>
2018-04-19 23:15:54 +0200

    generic updates useful to understand the match flow

--- src/main/java/it/polimi/ingsw/model/gameobjects/Match.java
+++ src/main/java/it/polimi/ingsw/model/gameobjects/Match.java
@@ -1,6 +1,20 @@
 package it.polimi.ingsw.model.gameobjects;
 
 public class Match {
-   
+    List<Player> players;
+    DecksContainer decksContainer;
+    Bag bag;
+    Board board;
+
+    public Match(List<Player> players) {
+        this.players=players;//or not? to understand what's the relationship with Room
+        this.decksContainer = new DecksContainer();
+        this.bag = new Bag(18);
+        this.board=new Board(this,players);
+    }
+
+    public DecksContainer getDecksContainer() {
+        return decksContainer;
+    }
 }
 
