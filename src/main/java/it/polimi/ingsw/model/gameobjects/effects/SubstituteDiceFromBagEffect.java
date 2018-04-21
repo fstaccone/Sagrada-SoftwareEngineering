a1cc645bd702185ff93065ca5db12b136b5ccc26
GuidoSergi <guido.sergi@mail.polimi.it>
2018-04-21 19:02:46 +0200

    SubstituteDiceFromBagEffect

--- src/main/java/it/polimi/ingsw/model/gameobjects/effects/SubstituteDiceFromBagEffect.java
+++ src/main/java/it/polimi/ingsw/model/gameobjects/effects/SubstituteDiceFromBagEffect.java
@@ -1,7 +1,11 @@
 package it.polimi.ingsw.model.gameobjects.effects;
 
+import it.polimi.ingsw.model.gameobjects.Dice;
 import it.polimi.ingsw.model.gameobjects.Match;
 import it.polimi.ingsw.model.gameobjects.Player;
+import it.polimi.ingsw.model.gameobjects.WindowPatternCard;
+
+import java.util.Scanner;
 
 public class SubstituteDiceFromBagEffect implements Effect {
 
@@ -10,6 +14,36 @@ public class SubstituteDiceFromBagEffect implements Effect {
 
     @Override
     public void applyEffect(Player player, Match match) {
-
+        if(player.getPickedDice()==null)
+            player.setPickedDice(match.getBoard().getReserve().chooseDice());
+        match.getBag().riponi(player.getPickedDice());
+        Dice diceFromBag = match.getBag().pesca(1).remove(0);
+        System.out.println("You got this dice: "+diceFromBag.toString());
+        Scanner scan = new Scanner(System.in);
+        int token = 0;
+        while(token ==0){
+            System.out.println("Please choose a value:");
+            int value = scan.nextInt();
+            if(value>0 && value<7){
+                diceFromBag.setValue(value);
+                player.setPickedDice(diceFromBag);
+                token = 1;
+            }
+        }
+        Dice dice = player.getPickedDice();
+        int result = 0;
+        WindowPatternCard schema = player.getSchemeCard();
+        while(result==0) {
+            System.out.println("Please put row where you want to move your dice: ");
+            int newRow = scan.nextInt();
+            System.out.println("Please put column where you want to move your dice: ");
+            int newColumn = scan.nextInt();
+            schema.putDice(dice, newRow, newColumn);
+            //Checking if putDice worked, else get a different new position
+            if(dice.equals(schema.getWindow()[newRow][newColumn].getDice()))
+                result = 1;
+            else System.out.println("Please choose a different position.");
+        }
+        player.setSchemeCard(schema);
     }
 }
