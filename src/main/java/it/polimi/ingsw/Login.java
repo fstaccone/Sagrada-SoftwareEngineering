db1cc97a11516e642717ee4dd63097c16c928dca
francescostaccone <francesco0staccone>
2018-05-05 01:19:19 +0200

    created Login.class that works with login-sagrada.fxml, to be continued

---
+++ src/main/java/it/polimi/ingsw/Login.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+
+public class Login extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(new URL("File:./src/main/java/it/polimi/ingsw/resources/login-sagrada.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Sagrada");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+
+
+    }
+
+}
