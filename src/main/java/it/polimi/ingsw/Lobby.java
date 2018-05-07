e4def10c0f94424ba30a0f8c4e90a46aedda5069
romeopaolo <paolo1.romeo@mail.polimi.it>
2018-05-07 14:00:49 +0200

    timer im multiplayer match creation (to be completed)

--- src/main/java/it/polimi/ingsw/Lobby.java
+++ src/main/java/it/polimi/ingsw/Lobby.java
@@ -13,16 +13,18 @@ public class Lobby {
     private List<String> takenUsernames;
     private int matchCounter;
     private List<String> waitingPlayers;
+    private int waitingTime;
     // map that contains the link between a client(only multiplayer clients) and the matchId useful for reconnection
     private Map<String,Integer> mapClientsToRoom;
     private List<MatchMultiplayer> multiplayerMatches;
 
-    public Lobby() {
+    public Lobby(int waitingTime) {
         this.takenUsernames = new ArrayList<>();
         this.matchCounter = 0;
         this.waitingPlayers = new ArrayList<>();
         this.mapClientsToRoom = new HashMap<>();
         this.multiplayerMatches = new ArrayList<>();
+        this.waitingTime = waitingTime;
     }
 
     public List<String> getTakenUsernames() {
@@ -53,26 +55,52 @@ public class Lobby {
         System.out.println("Match multiplayer creato: " + (matchCounter));
     }
 
-    public synchronized void addToWaitingPlayers(String name){
+    public synchronized void addToWaitingPlayers(String name) {
 
-        if(waitingPlayers.size() == 0){
-            // vuol dire che è una nuova partita dunque fa partire il timer
+        if(waitingPlayers.size() == 1){
+            this.waitingPlayers.add(name);
+            checkForStarting(this.waitingTime);
         }
-        this.waitingPlayers.add(name);
-        this.waitingPlayers.stream().forEach(p -> System.out.println(p));
-        checkForStarting();
+       else{
+            this.waitingPlayers.add(name);
+        }
+        // just for debug
+        this.waitingPlayers.forEach(p -> System.out.println(p));
     }
 
     // deve fare anche il controllo sul timer
-    public void checkForStarting(){
-        if (waitingPlayers.size() == 4){
-            for (String name: waitingPlayers) {
-                mapClientsToRoom.put(name,matchCounter);
+    public void checkForStarting(int waitingTime) {
+
+        if(waitingPlayers.size() > 1) {
+            int timer = 0;
+            // just for debug
+            System.out.println("Timer started!");
+            while (waitingPlayers.size() < 4 && timer < (waitingTime / 1000)) {
+                try {
+                    Thread.sleep(1000);
+                    timer++;
+                    System.out.println(timer + "s of " + waitingTime/1000 );
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println("Problem in sleeping time while updating waiting time in Lobby");
+                }
             }
-            createMultiplayerMatch(waitingPlayers);
-            matchCounter++;
-            waitingPlayers = new ArrayList<>();
+            startMatch();
         }
+    }
+
+    // potrebbe controllare che tutti i giocatori aggiunti siano connessi prima di creare la partita
+    // in caso di disconnessione gli fa passare il turno
+    // in questo modo possiamo gestire la rinuncia alla partita semplicemente con un metodo removePlayer se il client
+    // chiude prima che la partita sia iniziata
+    private void startMatch(){
+        // links between client and match are registered into the map
+        for (String name: waitingPlayers) {
+            mapClientsToRoom.put(name,matchCounter);
+        }
+        createMultiplayerMatch(waitingPlayers);
+        matchCounter++;
+        waitingPlayers = new ArrayList<>();
     }
 
 }
